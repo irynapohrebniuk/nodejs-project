879fb662f512e1731738f9576af6585b22163223
Iryna Pohrebniuk <iryna.pohrebniuk@gmail.com>
2020-11-03 23:46:48 +0100

    Add styles to the html pages

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
 app.use(userRoutes)
--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,6 @@ router.get('/add-product', (req, res, next) => {
 })
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body)
     res.redirect('/')
 })
 
